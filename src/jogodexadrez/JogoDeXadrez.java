11895dbef75a1fad8ab4df70733a5a20d59172b9
Gabriel Bessa <bessagabriel490@gmail.com>
2020-07-21 15:54:39 -0300

    Criacao da classe posicao.

--- src/jogodexadrez/JogoDeXadrez.java
+++ src/jogodexadrez/JogoDeXadrez.java
@@ -1,9 +1,12 @@
 package jogodexadrez;
 
-public class JogoDeXadrez {
+import CamadaTabuleiro.Posicao;
 
+public class JogoDeXadrez {
+    // Jogo De Xadrez Desenvolvido por GABRIEL BESSA segundo as aulas da UDEMY - JAVA
     public static void main(String[] args) {
-    
+        Posicao pos = new Posicao(2, 3);
+        System.out.println(pos);
     }
     
 }
