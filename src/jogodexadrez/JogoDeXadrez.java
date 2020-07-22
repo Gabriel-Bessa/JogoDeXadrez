14f5fdf5e8fe4590cbd34cba2a9c94fc05234fe6
Gabriel Bessa <bessagabriel490@gmail.com>
2020-07-22 00:26:04 -0300

    Inicio da criação do tabuleiro e Peças

--- src/jogodexadrez/JogoDeXadrez.java
+++ src/jogodexadrez/JogoDeXadrez.java
@@ -1,12 +1,11 @@
 package jogodexadrez;
 
-import CamadaTabuleiro.Posicao;
+import CamadaTabuleiro.Tabuleiro;
 
 public class JogoDeXadrez {
     // Jogo De Xadrez Desenvolvido por GABRIEL BESSA segundo as aulas da UDEMY - JAVA
     public static void main(String[] args) {
-        Posicao pos = new Posicao(2, 3);
-        System.out.println(pos);
+       Tabuleiro tab = new Tabuleiro(8, 8);
     }
     
 }
