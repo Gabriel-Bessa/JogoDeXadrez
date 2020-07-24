b9c285114b44312386a40699735912fe7b389be5
Gabriel Bessa <bessagabriel490@gmail.com>
2020-07-24 00:26:59 -0300

    Tratamento de Exceções

--- src/jogodexadrez/JogoDeXadrez.java
+++ src/jogodexadrez/JogoDeXadrez.java
@@ -1,5 +1,6 @@
 package jogodexadrez;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
@@ -11,15 +12,26 @@ public class JogoDeXadrez {
         Scanner sc = new Scanner(System.in);
         PartidaXadrez partidaXadrez = new PartidaXadrez();
         while(true){
-            UI.printTabuleiro(partidaXadrez.getPecas());
-            System.out.println();
-            System.out.print("Posição de Origem: ");
-            PosicaoXadrez inicial = UI.leitorPosicaoXadrez(sc);
-            System.out.println("");
-            System.out.print("Posição de destino ");
-            PosicaoXadrez alvo = UI.leitorPosicaoXadrez(sc);
-            PecaXadrez pecaCapturada = partidaXadrez.movimentacaoDePeca(inicial, alvo);
-            System.out.println("Peças Capturadas: "+pecaCapturada);
+            try{                
+                UI.printTabuleiro(partidaXadrez.getPecas());
+                System.out.println();
+                System.out.print("Posição de Origem: ");
+                PosicaoXadrez inicial = UI.leitorPosicaoXadrez(sc);
+                System.out.println("");
+                System.out.print("Posição de destino ");
+                PosicaoXadrez alvo = UI.leitorPosicaoXadrez(sc);
+                PecaXadrez pecaCapturada = partidaXadrez.movimentacaoDePeca(inicial, alvo);
+                System.out.println("Peças Capturadas: "+pecaCapturada);
+                UI.clearScreen();
+            }
+            catch (xadrez.xadrezException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
     
